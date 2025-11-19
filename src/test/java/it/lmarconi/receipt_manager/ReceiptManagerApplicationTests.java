1f8d4e96e51316c5adf92a6d1a31558709c40826
loremarconi <73281202+loremarconi@users.noreply.github.com>
2025-11-19 22:48:35 +0100

    First commit - Empty Spring Boot project
    Empty Spring Boot project generated with Spring Initializr (Java 21, Spring Boot 3.5.7, Maven).

---
+++ src/test/java/it/lmarconi/receipt_manager/ReceiptManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.lmarconi.receipt_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
